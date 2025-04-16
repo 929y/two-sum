87f2b70f476eba1802bc7f47afc724faa256cc9e
929y <sean0929ys@gmail.com>
2025-04-16 18:48:43 +0900

    initial version of main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def twoSum(nums:List[int], target: int):
+    flag = 1
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] == target :
+                flag = 0
+                break
+        if flag == 0:
+            break
+    output = [i,j]
+    return output
+
+nums = [3, 2, 4]
+target = 6
+print(twoSum(nums, target))
